0e752ae84a572228e1ab3fd411d8ba0442f290eb
wit <what.it.takes.to.business@gmail.com>
2021-09-08 01:32:12 +0900

    mod: トップ画面変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Semantic UI Documentation`,
-    description: `The style guide and component library for Semantic UI`,
-    author: `@whoisryosuke`,
+    title: `AL SYS`,
+    description: `Active Learning System`,
+    author: `@wit`,
     sidebar: {
       pages: [
         {
@@ -176,18 +176,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     `gatsby-plugin-mdx`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-less`,
